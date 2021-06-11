9550a63b7b5454d142d2f5d53a6f0d5d09d23ae1
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2021-06-11 19:13:25 +0545

    xd

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	bot: {
+		token: 'B0T.T0K3N.H3R3',
+		prefix: '!',
+		clientSecret: 'CLIENT_SECRET',
+		clientID: 'CLIENT_ID'
+	},
+	website: {
+		protocol: 'https://',
+		domain: 'domain.name',
+		port: 8080,
+		captcha: {
+			sitekey: 'SITE_KEY',
+			secretkey: 'SECRET_KEY'
+		}
+	}
+};
